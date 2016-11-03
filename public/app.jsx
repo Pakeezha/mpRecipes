9265174cdb8ffe9f845bad70e77e6dcc0aebeef1
Maleeha Farukh <maleehafarukh@Maleehas-MBP.byod.gmu.edu>
2016-11-03 11:12:46 -0400

    adding hw8 files

---
+++ public/app.jsx
@@ -0,0 +1,69 @@
+//This file adds React
+
+
+//Using react to create a menu bar using React and Bootstrap
+var MyReactBootstrapButton = React.createClass({
+    render: function() {
+
+        var ButtonGroup = ReactBootstrap.ButtonGroup,
+            Button  = ReactBootstrap.Button;
+
+        return (<div>
+            <ButtonGroup>
+                <Button id= "landing" bsStyle="success" href="landing.html">Home </Button>
+                <Button id= "login" bsStyle="success" href="login.html">Login</Button>
+                <Button id= "bucket" bsStyle="success" href="bucket.html">Bucket</Button>
+                <Button id= "trending" bsStyle="success" href="trending.html">Trending</Button>
+            </ButtonGroup>
+        </div>);
+    }
+});
+
+React.render(<MyReactBootstrapButton />, document.getElementById("container"));
+
+
+//Using React to add a link to our github page
+var ProductItem = React.createClass({
+    render: function () {
+        return (
+            <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.price}</td>
+            </tr>
+        );
+    }
+});
+
+class Avatar extends React.Component {
+    render() {
+        return (
+            <div>
+                <PagePic pagename={this.props.pagename} />
+                <PageLink pagename={this.props.pagename} />
+            </div>
+        );
+    }
+}
+
+class PagePic extends React.Component {
+    render() {
+        return (
+            <img src={'image/' + this.props.pagename + '/GitHub_logo.png'} />
+        );
+    }
+}
+
+class PageLink extends React.Component {
+    render() {
+        return (
+            <a href={'https://github.com/Pakeezha/' + this.props.pagename}>
+                {this.props.pagename}
+            </a>
+        );
+    }
+}
+
+React.render(
+    <Avatar pagename="RecipeBucket" />,
+    document.getElementById('git')
+);
